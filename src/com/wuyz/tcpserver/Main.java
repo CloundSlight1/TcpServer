8ed3bbcf802d4ca36ff01698066f234c49db1f19
wuyuz <wuyuz@landicorp.com>
2016-10-20 21:45:01 +0800

    corrent the ip message

--- src/com/wuyz/tcpserver/Main.java
+++ src/com/wuyz/tcpserver/Main.java
@@ -1,6 +1,7 @@
 package com.wuyz.tcpserver;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.MessageDigest;
@@ -11,7 +12,7 @@ public class Main {
     static final char[] HEX_CHAR = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     private static String name;
-    private static String md5;
+    private static String sha1;
     private static long length;
 
     public static void main(final String[] args) {
@@ -29,17 +30,17 @@ public class Main {
         name = file.getName();
         length = file.length();
         try (FileInputStream inputStream = new FileInputStream(file)) {
-            md5 = getMd5(inputStream);
+            sha1 = getSHA1(inputStream);
         } catch (Exception e) {
             e.printStackTrace();
             return;
         }
-        System.out.println(String.format(Locale.getDefault(), "name: %s, length: %s(0x%X), md5: %s",
-                name, getLengthDesc(length), length, md5));
+        System.out.println(String.format(Locale.getDefault(), "name: %s, length: %s(0x%X), sha1: %s",
+                name, getLengthDesc(length), length, sha1));
 
         try {
             ServerSocket serverSocket = new ServerSocket(9999, 10);
-            System.out.println("ip: " + serverSocket.getInetAddress());
+            System.out.println("ip: " + InetAddress.getLocalHost().getHostAddress() + ":" + serverSocket.getLocalPort());
             while (true) {
                 System.out.println("Waiting for client ...");
                 final Socket socket = serverSocket.accept();
@@ -110,7 +111,7 @@ public class Main {
         return String.format(Locale.getDefault(), "%.1fG", length);
     }
 
-    public static String getMd5(String input) {
+    public static String getSHA1(String input) {
         if (input == null || input.isEmpty())
             return null;
         try {
@@ -130,11 +131,11 @@ public class Main {
         }
     }
 
-    public static String getMd5(InputStream input) {
+    public static String getSHA1(InputStream input) {
         byte[] data = new byte[1024];
         int n;
         try {
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            MessageDigest mdInst = MessageDigest.getInstance("SHA-1");
             while ((n = input.read(data)) > 0) {
                 mdInst.update(data, 0, n);
             }
@@ -185,10 +186,10 @@ public class Main {
         outputStream.write(buffer);
         System.out.println("send length end");
 
-        System.out.println("send md5 begin");
-        buffer = md5.getBytes();
+        System.out.println("send sha1 begin");
+        buffer = sha1.getBytes();
         outputStream.write(buffer.length);
         outputStream.write(buffer);
-        System.out.println("send md5 end");
+        System.out.println("send sha1 end");
     }
 }
